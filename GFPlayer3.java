55222571f3758933ac861b83c180c05a73d0231e
simone <55714164+Follisim@users.noreply.github.com>
2022-08-20 16:31:43 +0200

    Update GFPlayer3.java

--- GFPlayer3.java
+++ GFPlayer3.java
@@ -2,16 +2,17 @@ package mnkgame;
 
 import java.util.HashSet;
 import java.util.concurrent.TimeUnit;
-import java.util.Random;
+
 
 public class GFPlayer3 implements MNKPlayer{
 
-    private Random rand;
     private int N;
     private int M;
     private int K;
+    private Board B;
     private MNKCellState Me;
     private MNKCellState Enemy;
+
     private int TIMEOUT;
     private boolean first;
     private int Alpha;
@@ -21,44 +22,41 @@ public class GFPlayer3 implements MNKPlayer{
 
 public GFPlayer3(){}
 
-
 public void initPlayer(int M, int N, int K, boolean first, int timeout_in_secs) {
 
-    rand = new Random(System.currentTimeMillis());
     TIMEOUT = timeout_in_secs;
     this.first = first;
     Me = first ? MNKCellState.P1 : MNKCellState.P2;
     Enemy = first ? MNKCellState.P2 : MNKCellState.P1;
+
+    this.B = new Board(M, N, K, first);
+
     this.M = M;
     this.N = N;
     this.K = K;
-}
 
+}
 
 public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
     long start = System.currentTimeMillis(); // gestione del tempo ancora da fare
-    Board B = new Board(M, N, K); // tabella allo stato attuale 
 
+    
     if (MC.length > 0) {
         Boolean t = first;
         for (int w = 0; w < MC.length; w++) {
-            B.markCell(MC[w].i, MC[w].j, t); // copy all moves in B
+            B.markCell(MC[w].i, MC[w].j, t); // copy oll moves in B
             t = !t;
+
         }
-        //recupero l'ultima mossa del nemico?
-        
-    }
-        // prima mossa causale
-		if (MC.length == 0) {
-			MNKCell c = FC[rand.nextInt(FC.length)];
-			B.markCell(c.i, c.j, true);
-			return c;
-		}// first move
+    } else {// first move
         // fast selection
-    
+    }
+
     // last move
     if (FC.length == 1)
         return FC[0];
+
+
   
     //  libere HashSet
     HashSet<MNKCell> H = new HashSet<MNKCell>((int) 21 );
@@ -66,81 +64,104 @@ public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC) {
         H.add(FC[i]);
         if (B.isWinningCell(FC[i].i, FC[i].j, Me))
                 return FC[i];
+        if (B.isWinningCell(FC[i].i, FC[i].j, Enemy))
+                return FC[i];
     }
+
     // occupate HashSet
     HashSet<MNKCell> Z = new HashSet<MNKCell>((int) 21);
     for (int i = 0; i < MC.length; i++) {
         Z.add(MC[i]);
     }
-
+    
     CellaValore finalCell = new CellaValore();
     finalCell.cell = FC[0];
     finalCell.val = 0;
 
-    for(MNKCell cell : H){
+    for(int i=0; i < FC.length; i++){
 
+        
+        B.markCell(FC[i].i, FC[i].j, true);
 
-        B.markCell(cell.i, cell.j, true);
-        double e = AlphaBeta(H, B,false, -10, 10, 0);
-        B.freeCell(cell.i, cell.j);
+        double e = AlphaBeta(H, B,false, -100, 100, 10);
 
-         CellaValore h = new CellaValore();
-        h.cell = cell;
+        B.freeCell(FC[i].i, FC[i].j);
+
+        CellaValore h = new CellaValore();
+        h.cell = FC[i];
         h.val = e;
 
         if(finalCell.val < h.val)
             finalCell = h;
-
+       
     }
 
     return finalCell.cell;
 }
 
 public String playerName() {
-    return "GFPlayer3";
+    return "GFPlayer2";
 }
 
-/*---------------------------------------------------------------------------------------------------- */
 
+
+/*---------------------------------------------------------------------------------------------------- */
 
 public class CellaValore {
     double val;
     MNKCell cell;
 }
 
+
 public int evaluate(Board B, MNKCell c) {
 
     if (B.isWinningCell(c.i, c.j, Me)) {
+
         return 1;
     } else if (B.isWinningCell(c.i, c.j, Enemy)) {
+
         return -1;
     } else
+
         return 0;
+
 }
+
+
 // lo trasformiamo in coda con metodo fromArrayToQueue, e lanciamo alphabeta
+
 public int AlphaBeta(HashSet<MNKCell> H,Board B, boolean t, int Alpha, int Beta, int depth) {
     // System.out.println(Q.size());
     int ev;
     int t_ev;
     if (H.size() == 1 || depth == MAX_DEPTH) {
-        return valutazione euristica ;        
+         MNKCell[] cells = H.toArray(new MNKCell[H.size()]);
+        return evaluate(B, cells[0]) ;
     }
-    if (t) { // P1 massimizza
+
+    if (t) { //  massimizza
        // se ho vinto ritorno + 100/depth
+
         ev = -100;
         t_ev = -100;
         MNKCell[] cells = H.toArray(new MNKCell[H.size()]);
+
         for (int i = 0; i < cells.length ; i++) {
             MNKCell c = cells[i];
             B.markCell(c.i, c.j, true);
+            if(B.MyWin) // tentativo di valutazione in base alla profondità
+                t_ev = Math.max(t_ev, 10/depth);
             H.remove(c);
+
             ev = Math.max(ev, AlphaBeta(H, B, false, Alpha, Beta, depth + 1));
+            
             t_ev= Math.max(t_ev, ev);
             Alpha = Math.max(Alpha, t_ev);
             B.freeCell(c.i, c.j);
             H.add(c);
             if (Beta <= Alpha)
                 break;
+
         // il risultatp deve essere diviso per un fattore profondita 
         //per favorire i cammini brevi 
         // es. lose = 100/depth
@@ -150,10 +171,14 @@ public int AlphaBeta(HashSet<MNKCell> H,Board B, boolean t, int Alpha, int Beta,
         ev = 100;
         t_ev = 100;
         MNKCell[] cells = H.toArray(new MNKCell[H.size()]);
+
         for (int i = 0; i < cells.length; i++) {
             MNKCell c = cells[i];
             B.markCell(c.i, c.j, false);
+            if (B.EnemyWin) // tentativo di valutazione in base alla profondità
+                t_ev = Math.min(t_ev, -(10 / depth));
             H.remove(c);
+
             ev = Math.min(ev, AlphaBeta(H, B, true, Alpha, Beta, depth + 1));
             t_ev = Math.min(t_ev, ev);
             Beta = Math.min(Beta, t_ev);
@@ -165,5 +190,6 @@ public int AlphaBeta(HashSet<MNKCell> H,Board B, boolean t, int Alpha, int Beta,
     }
     return t_ev;
 }
+
 }
     
